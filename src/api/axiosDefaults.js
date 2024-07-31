4d0699ea269c1ed1c7978f37ff098987e3d0e88e
David Bergqvist <David.bergqvist@gmail.com>
2024-07-31 07:14:31 +0000

    solved node error

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-app-2024-29cb82ccb5b6.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
